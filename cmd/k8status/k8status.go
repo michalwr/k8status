0e73dd2dc34552b73d93f59e140d800709add062
Michal.Wrona <michal.wrona@gmail.com>
2021-11-16 23:11:09 +0100

    init

---
+++ cmd/k8status/k8status.go
@@ -0,0 +1,7 @@
+package main
+import(
+	"github.com/michalwr/k8status/internal/draw"
+)
+func main() {
+    draw.MainLoop()
+}
---
+++ internal/draw/mainLoop.go
@@ -0,0 +1,37 @@
+package draw
+import	(
+	"log"
+	"github.com/gdamore/tcell/v2"
+
+)
+func MainLoop(){
+
+
+
+	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+	s, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	if err := s.Init(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+	s.SetStyle(defStyle)
+	s.EnableMouse()
+	s.EnablePaste()
+	s.Clear()
+	for {
+		s.Show()
+		ev := s.PollEvent()
+		switch ev := ev.(type) {
+		case *tcell.EventResize:
+			s.Sync()
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+				//quit()
+			} else if ev.Key() == tcell.KeyCtrlL {
+				s.Sync()
+			}
+		}
+	}
+}
